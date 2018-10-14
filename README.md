47b346bf68c39fefe1883b6acf9538f05d200ecd
Diogo Paza <diogopazacvel@gmail.com>
2018-10-14 16:55:20 -0300

    example

--- README.md
+++ README.md
@@ -8,4 +8,13 @@ import cv2
 
 imagem = cv2.imread("jogador.jpg")
 altura, largura = imagem.shape[:2]
-cv2.imshow("janela", imagem)
+ponto_inicial = cv2.getRotationMatrix2D( (largura, altura), 30,1)
+
+imagem_rotacionada = cv2.warpAffine( imagem, ponto_inicial, (largura,altura))
+ 
+cv2.imshow("janela", imagem_rotacionada)
+
+<h3>Exemplo2:</h3>
+
+
+
--- exemplo1.py
+++ exemplo1.py
@@ -1,13 +1,24 @@
+"""
 import cv2
 
 imagem = cv2.imread("jogador.jpg")
 altura, largura = imagem.shape[:2]
 print(altura)
 print(largura)
-ponto_inicial = cv2.getRotationMatrix2D((0,0), 30, 1)
+ponto_inicial = cv2.getRotationMatrix2D((largura,altura), 30, 1)
                     
 imagem_rotacionada =  cv2.warpAffine(imagem, ponto_inicial, (largura, altura))
 
 cv2.imshow("imagem rotacionada", imagem_rotacionada)
 
 #cv2.imshow("janela", imagem)
+"""
+import cv2
+
+imagem = cv2.imread("jogador.jpg")
+altura, largura = imagem.shape[:2]
+ponto_inicial = cv2.getRotationMatrix2D( (largura, altura), 30,1)
+
+imagem_rotacionada = cv2.warpAffine( imagem, ponto_inicial, (largura,altura))
+ 
+cv2.imshow("janela", imagem_rotacionada)
